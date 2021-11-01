a1f60bd26f070c4d4492596533be66317429d64d
Mathias Jensen <cph-mj839@chpbusiness.dk>
2021-11-01 15:38:39 +0100

    end of day

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const info = [
+  {id: "rendering", title:"Rendering with React",info:"Add some text here"},
+  {id: "components", title:"components",info:"Add some text here"},
+  {id: "props-v-state", title:"Props v. State",info:"Add some text here"}
+]
+
+ReactDOM.render(<App info={info} />, document.getElementById('root'));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
